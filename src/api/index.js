1f4afee0dfcf8c8113932bbf05f30b55539765d1
fourdusk <mikeqiluo@163.com>
2019-06-18 12:19:11 +0800

    change api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,102 +1,85 @@
 import axios from 'axios'
-const baseURL = 'http://121.41.18.59:3000/'
+const baseURL = 'http://121.41.18.59:3000'
 export default {
   netease: {
     async getRecommendList(page, pagesize) {
       let url =
-        '/api/netease/recommendlist/' + String(page) + '/' + String(pagesize)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+        baseURL +
+        '/api/netease/recommendlist/' +
+        String(page) +
+        '/' +
+        String(pagesize)
+      return await axios.get(url)
     },
     async getNewList() {
-      let url = '/api/netease/newlist'
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/netease/newlist'
+      return await axios.get(url)
     },
     async getHotList() {
-      let url = '/api/netease/hotlist'
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/netease/hotlist'
+      return await axios.get(url)
     },
     async getSongList(id) {
-      let url = '/api/netease/songlist/' + String(id)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/netease/songlist/' + String(id)
+      return await axios.get(url)
     },
     async getSongDetail(ids) {
-      let url = '/api/netease/songdetail/' + String(ids)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/netease/songdetail/' + String(ids)
+      return await axios.get(url)
     }
   },
   qq: {
     async getRecommendList(page, pagesize) {
-      let url = '/api/qq/recommendlist/' + String(page) + '/' + String(pagesize)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url =
+        baseURL +
+        '/api/qq/recommendlist/' +
+        String(page) +
+        '/' +
+        String(pagesize)
+      return await axios.get(url)
     },
     async getNewList() {
-      let url = '/api/qq/newlist'
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/qq/newlist'
+      return await axios.get(url)
     },
     async getHotList() {
-      let url = '/api/qq/hotlist'
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/qq/hotlist'
+      return await axios.get(url)
     },
     async getSongList(id) {
-      let url = '/api/qq/songlist/' + String(id)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/qq/songlist/' + String(id)
+      return await axios.get(url)
     },
     async getSongDetail(ids) {
-      let url = '/api/qq/songdetail/' + String(ids)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/qq/songdetail/' + String(ids)
+      return await axios.get(url)
     }
   },
   kugou: {
     async getRecommendList(page, pagesize) {
       let url =
-        '/api/kugou/recommendlist/' + String(page) + '/' + String(pagesize)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+        baseURL +
+        '/api/kugou/recommendlist/' +
+        String(page) +
+        '/' +
+        String(pagesize)
+      return await axios.get(url)
     },
     async getNewList() {
-      let url = '/api/kugou/newlist'
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/kugou/newlist'
+      return await axios.get(url)
     },
     async getHotList() {
-      let url = '/api/kugou/hotlist'
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/kugou/hotlist'
+      return await axios.get(url)
     },
     async getSongList(id) {
-      let url = '/api/kugou/songlist/' + String(id)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/kugou/songlist/' + String(id)
+      return await axios.get(url)
     },
     async getSongDetail(ids) {
-      let url = '/api/kugou/songdetail/' + String(ids)
-      return await axios.get(url, { baseURL: baseURL }).catch(error => {
-        console.log(error)
-      })
+      let url = baseURL + '/api/kugou/songdetail/' + String(ids)
+      return await axios.get(url)
     }
   }
 }
